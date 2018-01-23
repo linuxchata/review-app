bb789a13a96e6c42088f98ecbc013c334a04db39
linuxchata <linuxchata@hotmail.com>
2018-01-23 20:52:08 +0100

    Implement test for sync location service

--- src/ReviewSystem.Services.Tests/LocationSynchronizationServiceTests.cs
+++ src/ReviewSystem.Services.Tests/LocationSynchronizationServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Moq;
+using ReviewSystem.Core.Domain;
 using ReviewSystem.Services.Contracts;
 using ReviewSystem.Services.Synchronization;
 using Xunit;
@@ -10,29 +11,35 @@ namespace ReviewSystem.Services.Tests
     {
         private readonly Mock<IWikipediaService> wikipediaServiceMock;
 
+        private readonly Mock<ILocationService> locationService;
+
         public LocationSynchronizationServiceTests()
         {
             this.wikipediaServiceMock = new Mock<IWikipediaService>();
             this.wikipediaServiceMock
                 .Setup(a => a.GetPageContent())
-                .Returns(Task.FromResult(string.Empty));
+                .Returns(Task.FromResult(Properties.Resources.PageContent));
+
+            this.locationService = new Mock<ILocationService>();
         }
 
-        // [Fact]
+        [Fact]
         public void Synchronize_WhenContentIsNotEmpty_ShouldNotThrowException_Test()
         {
             // Arrange
             var parsingService = new WikipediaParsingService();
-            var locationService = new LocationService(null);
             var sut = new LocationSynchronizationService(
                 this.wikipediaServiceMock.Object,
                 parsingService,
-                locationService);
+                this.locationService.Object);
 
             // Act
             sut.Synchronize();
 
             // Assert
+            this.wikipediaServiceMock.Verify(a => a.GetPageContent(), Times.Once);
+            this.locationService.Verify(a => a.GetAllAsync(), Times.Once);
+            this.locationService.Verify(a => a.CreateAsync(It.IsAny<Location>(), It.IsAny<string>()), Times.AtLeastOnce);
         }
     }
 }
