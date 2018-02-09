213ba79588e698789fbd991d77f7a92604525172
linuxchata <linuxchata@hotmail.com>
2018-02-09 17:51:44 +0100

    Revert back sync solution

---
+++ src/Synchronization/LC.RS.Synchronization/AutofacModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+
+namespace LC.RS.Synchronization
+{
+    public sealed class AutofacModule : Module
+    {
+        private readonly IConfiguration configuration;
+
+        public AutofacModule(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
