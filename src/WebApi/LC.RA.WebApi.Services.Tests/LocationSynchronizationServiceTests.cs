e0ae9b6b47c158068487437f2ad13203493a9de4
linuxchata <linuxchata@hotmail.com>
2018-02-20 19:59:41 +0100

    Web Api: Fix location service sync unit tests

--- src/WebApi/LC.RA.WebApi.Services.Tests/LocationSynchronizationServiceTests.cs
+++ src/WebApi/LC.RA.WebApi.Services.Tests/LocationSynchronizationServiceTests.cs
@@ -10,17 +10,10 @@ namespace LC.RA.WebApi.Services.Tests
 {
     public sealed class LocationSynchronizationServiceTests
     {
-        private readonly Mock<IWikipediaService> wikipediaServiceMock;
-
         private readonly Mock<ILocationService> locationServiceMock;
 
         public LocationSynchronizationServiceTests()
         {
-            this.wikipediaServiceMock = new Mock<IWikipediaService>();
-            this.wikipediaServiceMock
-                .Setup(a => a.GetPageContent())
-                .Returns(Task.FromResult(Properties.Resources.PageContent));
-
             this.locationServiceMock = new Mock<ILocationService>();
         }
 
@@ -28,17 +21,16 @@ namespace LC.RA.WebApi.Services.Tests
         public void Synchronize_WhenContentIsNotEmpty_ShouldNotThrowException_Test()
         {
             // Arrange
-            var parsingService = new WikipediaParsingService();
-            var sut = new LocationSynchronizationService(
-                this.wikipediaServiceMock.Object,
-                parsingService,
-                this.locationServiceMock.Object);
+            IEnumerable<Location> locations = new List<Location>
+            {
+                new Location("Kiev", "Kiev")
+            };
+            var sut = new LocationSynchronizationService(this.locationServiceMock.Object);
 
             // Act
-            sut.Synchronize(new List<Location>());
+            sut.Synchronize(locations);
 
             // Assert
-            this.wikipediaServiceMock.Verify(a => a.GetPageContent(), Times.Once);
             this.locationServiceMock.Verify(a => a.GetAllAsync(), Times.Once);
             this.locationServiceMock.Verify(a => a.CreateAsync(It.IsAny<Location>(), It.IsAny<string>()), Times.AtLeastOnce);
         }
--- src/WebApi/LC.RA.WebApi.Services/Synchronization/LocationSynchronizationService.cs
+++ src/WebApi/LC.RA.WebApi.Services/Synchronization/LocationSynchronizationService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using LC.RA.WebApi.Core.Application.Wikipedia;
 using LC.RA.WebApi.Core.Domain;
 using LC.RA.WebApi.Services.Contracts;
 
@@ -8,25 +7,15 @@ namespace LC.RA.WebApi.Services.Synchronization
 {
     public sealed class LocationSynchronizationService : ILocationSynchronizationService
     {
-        private readonly IWikipediaService wikipediaService;
-
-        private readonly IWikipediaParsingService wikipediaParsingService;
-
         private readonly ILocationService locationService;
 
-        public LocationSynchronizationService(
-            IWikipediaService wikipediaService,
-            IWikipediaParsingService wikipediaParsingService,
-            ILocationService locationService)
+        public LocationSynchronizationService(ILocationService locationService)
         {
-            this.wikipediaService = wikipediaService;
-            this.wikipediaParsingService = wikipediaParsingService;
             this.locationService = locationService;
         }
 
         public async void Synchronize(IEnumerable<Location> sourceLocations)
         {
-            // var source = await this.GetSourceLocations();
             var existedLocations = await this.GetExistedLocations();
 
             foreach (var location in sourceLocations)
@@ -37,52 +26,10 @@ namespace LC.RA.WebApi.Services.Synchronization
                 }
             }
         }
-
-        private async Task<IEnumerable<Location>> GetSourceLocations()
-        {
-            var pageContent = await this.wikipediaService.GetPageContent();
-
-            var parsedPageContent = this.wikipediaParsingService.ParsePage(pageContent);
-            var parsedTable = this.wikipediaParsingService.ParseTable(parsedPageContent);
-
-            var source = new List<Location>();
-            foreach (var row in parsedTable)
-            {
-                if (row is WikiTableRow)
-                {
-                    var location = new Location(this.GetName(row), this.GetRegion(row));
-                    source.Add(location);
-                }
-            }
-
-            return source;
-        }
-
+        
         private async Task<HashSet<Location>> GetExistedLocations()
         {
             return new HashSet<Location>(await this.locationService.GetAllAsync());
-        }
-
-        private string GetName(WikiTableRowBase row)
-        {
-            var nameMatches = RegexExtension.GetMatches(row.Content[0], RegexPattern.LocationNameMatchPattern);
-            var name = nameMatches[0].Groups[1].Value;
-
-            var nameCorrectedMatches = RegexExtension.GetMatches(name, RegexPattern.LocationNameCorretionMatchPattern);
-            if (nameCorrectedMatches.Count > 0)
-            {
-                var nameCorrected = nameCorrectedMatches[0].Groups[1].Value;
-                name = nameCorrected;
-            }
-
-            return name;
-        }
-
-        private string GetRegion(WikiTableRowBase row)
-        {
-            var regionMatches = RegexExtension.GetMatches(row.Content[1], RegexPattern.LocationRegionMatchPattern);
-            var region = regionMatches[0].Groups[1].Value;
-            return region;
         }
     }
 }
