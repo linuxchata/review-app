79322ef23c18ec0ba08466c44a50e8128ad75b44
linuxchata <linuxchata@hotmail.com>
2018-03-05 23:39:04 +0100

    Sync Service: Rearrange folders

--- src/Services/LC.RA.SynchronizationService.Api.Tests/Properties/Resources.Designer.cs
+++ src/Services/LC.RA.SynchronizationService.Api.Tests/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace LC.RA.SynchronizationService.Api.Tests.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace LC.RA.SynchronizationService.Api.Tests.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LC.RA.LocationService.Tests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LC.RA.SynchronizationService.Api.Tests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
