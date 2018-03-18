f4578b4e67bfaa1083755de080bfeb960297a6e2
linuxchata <linuxchata@hotmail.com>
2018-03-18 23:02:40 +0100

    Web: Add plugin to clean output

--- src/Web/webpack.config.js
+++ src/Web/webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/app.tsx',
-  watch: true,
+  watch: false,
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       hash: true,
       template: 'index.html',
