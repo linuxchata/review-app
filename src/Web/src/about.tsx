1c7fc0e54e5109418334cdb99b115b9142ca7c85
LinuxChata <linuxchata@hotmail.com>
2018-03-23 22:11:18 +0100

    Web: Rewrok about and no match components to Stateless Components

--- src/Web/src/about.tsx
+++ src/Web/src/about.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 
 import './styles/about.scss';
 
-class About extends React.Component<{}, {}>{
-  render() {
-    return (
-      <section className='about'>
-        <p>Copyright (C) 2018 Pylyp Lebediev</p>
-      </section>
-    )
-  }
+const About: React.StatelessComponent<{}> = () => {
+  return (
+    <section className='about'>
+      <p>Copyright (C) 2018 Pylyp Lebediev</p>
+    </section>
+  )
 }
 
 export default About;
--- src/Web/src/noMatch.tsx
+++ src/Web/src/noMatch.tsx
@@ -6,15 +6,13 @@ interface NoMatchProps {
   location: any;
 }
 
-class NoMatch extends React.Component<NoMatchProps, {}> {
-  render() {
-    return (
-      <div className='no-match'>
-        <p>Page not found</p>
-        <p>No matches for {this.props.location.pathname} path</p>
-      </div>
-    )
-  }
+const NoMatch: React.StatelessComponent<NoMatchProps> = () => {
+  return (
+    <div className='no-match'>
+      <p>Page not found</p>
+      <p>No matches for {this.props.location.pathname} path</p>
+    </div>
+  )
 }
 
 export default NoMatch;
