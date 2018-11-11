4776a2cefea7956efa33f7a71fb6257ba5d74e56
Pylyp Lebediev <linuxchata@gmail.com>
2018-11-11 23:34:58 +0100

    Fix namespace in resource file for location infrastructure tests

--- src/Services/Location/ReviewApp.Location.Infrastructure.Tests/Properties/Resources.Designer.cs
+++ src/Services/Location/ReviewApp.Location.Infrastructure.Tests/Properties/Resources.Designer.cs
@@ -36,7 +36,7 @@ namespace ReviewApp.Location.Infrastructure.Tests.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LC.RA.Location.Infrastructure.Tests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ReviewApp.Location.Infrastructure.Tests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
