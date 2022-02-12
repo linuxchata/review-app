fad25530c316e48546a298007c4229daf5a9d725
linuxchata <linuxchata@hotmail.com>
2022-02-12 22:44:04 +0100

    Update dependencies and regenerate certificates

--- install/https/generate_certificates.ps1
+++ install/https/generate_certificates.ps1
@@ -4,6 +4,6 @@ openssl x509 -outform der -in reviewapp-dev-ca.pem -out reviewapp-dev-ca.crt
 
 openssl genrsa -out dev.reviewapp.com.key 2048
 openssl req -new -key dev.reviewapp.com.key -subj "/CN=dev.reviewapp.com" -out dev.reviewapp.com.csr
-openssl x509 -req -in dev.reviewapp.com.csr -CA reviewapp-dev-ca.pem -CAkey reviewapp-dev-ca.key -CAcreateserial -out dev.reviewapp.com.crt -days 365 -sha256 -extfile reviewapp.ext
+openssl x509 -req -in dev.reviewapp.com.csr -CA reviewapp-dev-ca.pem -CAkey reviewapp-dev-ca.key -CAcreateserial -out dev.reviewapp.com.crt -days 1825 -sha256 -extfile reviewapp.ext
 openssl x509 -in dev.reviewapp.com.crt -out dev.reviewapp.com.pem -outform PEM
 openssl pkcs12 -export -in dev.reviewapp.com.crt -inkey dev.reviewapp.com.key -out dev.reviewapp.com.pfx
